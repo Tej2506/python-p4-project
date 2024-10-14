3c361b82086ab7c870511cbb2440cb68c1d0ac5e
Basavanna <basavannatej25@gmail.com>
2024-10-14 14:23:58 +0530

    routes for different components'

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,20 @@
+// components/App.js
+
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path = "/" element = {<Home/>}/>
+        <Route path = "/cars" element = {<Cars/>}/>
+        <Route path = "/signup" element = {<Signup/>}/>
+        <Route path = "/login" element = {<Login/>}/>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
