7ae0045b296c66aa6e182bf09f662926e7386bf2
Basavanna <basavannatej25@gmail.com>
2024-10-15 11:41:59 +0530

    added home layout to the website

---
+++ client/src/components/Home.js
@@ -0,0 +1,14 @@
+// components/Home.js
+
+import React from 'react';
+
+function Home(){
+    return(
+        <div>
+            <h1>Welcome to Car Compare</h1>
+            <p>This app allows you to compare different cars, view car details, and more. Please use the navigation above to explore the app</p>
+        </div>
+    );
+}
+
+export default Home;
