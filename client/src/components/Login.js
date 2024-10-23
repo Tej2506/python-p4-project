1896a8a4a5c5dbe2d12d1be1b60e429b4893b75b
Basavanna <basavannatej25@gmail.com>
2024-10-23 11:24:23 +0530

    updated login page with to handle invalid credentials

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,36 +1,51 @@
-//Login.js
+// Login.js
 import React from 'react';
 import { useFormik } from 'formik';
 import { useHistory } from 'react-router-dom';
-import {useState} from 'react';
-
-
+import { useState } from 'react';
 
 const Login = () => {
-    const history = useHistory()
-    const [loginSuccess, setLoginSuccess] = useState('')
-    const formik = useFormik({
-        initialValues: {
-        username: '',
-        password: ''
+  const history = useHistory();
+  const [loginFailed, setLoginFailed] = useState(false);
+  const [message, setMessage] = useState('');
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: ''
     },
     onSubmit: (values) => {
-        fetch('http://127.0.0.1:5000/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(values)
+      fetch('http://127.0.0.1:5000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+        body: JSON.stringify(values),
+      })
+        .then((res) => {
+          if (res.ok) {
+            if (res.status === 200) {
+              return res.json().then((data) => {
+                history.push(`/UserProfile/${data.user_id}`);
+              });
+            } else if (res.status === 201 || res.status === 202) {
+              return res.json().then((data) => {
+                setMessage(data['message']);
+                setLoginFailed(true);
+              });
+            }
+          } else {
+            return res.json().then((data) => {
+              setMessage(data['message']);
+              setLoginFailed(true);
+            });
+          }
         })
-            .then(res => {
-                if (res.ok){
-                    history.push(`/UserProfile/${res.user_id}`)
-                }
-                else{
-                    setLoginSuccess('Invalid credentials')
-                }
+        .catch((error) => {
+          console.error('Error during login:', error);
         });
-    }
+    },
   });
 
   return (
@@ -53,7 +68,7 @@ const Login = () => {
         />
         <button type="submit">Login</button>
       </form>
-      {loginSuccess &&(<p>{loginSuccess}</p>)}
+      {loginFailed && <p>{message}</p>}
     </div>
   );
 };
